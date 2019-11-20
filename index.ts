92edd5d3f0879953757d99f9df2f492dfd1fe848
JeroenKops <jeroen.kops@partners4it.nl>
2019-11-20 21:10:43 +0100

    Lesson 2 -finished

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 console.clear();
+
 import { Observable } from "rxjs";
+import {fromEvent} from 'rxjs';
+import { of } from 'rxjs';
+import { range } from 'rxjs';
+import { from } from 'rxjs';
+import { interval } from 'rxjs';
+import { timer} from 'rxjs';
 
 const observer = {
   next: value => console.log("next", value),
@@ -15,4 +22,44 @@ const observable = new Observable(subscriber => {
   subscriber.next("Hello");
 });
 
-observable.subscribe(observer);
+// observable.subscribe(observer);
+
+
+const source$ =  fromEvent(document,'click');
+
+//const subOne = source$.subscribe(observer);
+//const subTwo = source$.subscribe(observer);
+
+// setTimeout(()=>{
+//   console.log('unsubscribe from one');
+//   subOne.unsubscribe();
+//   },3000)
+
+const fromSoure$ = of(1,2,3,4,5);
+// fromSoure$.subscribe(observer);
+
+const rangeSource$ = range(1,9);
+// rangeSource$.subscribe(observer);
+
+const ofSource$ = of([1,2,3,4,5]);
+// ofSource$.subscribe(observer);
+
+const fromSource$ = from([1,2,3,4,5]);
+// fromSoure$.subscribe(observer);
+
+function* hello(){
+  yield 'Hello';
+  yield 'World'
+}
+
+const fromIteratorSource$ = from(hello());
+// fromIteratorSource$.subscribe(observer);
+
+const fromUrlSource$ = from(fetch('https://api.github.com/users/octocat'));
+// fromUrlSource$.subscribe(observer);
+
+const intervalSource$ = interval(1000);
+// intervalSource$.subscribe(console.log);
+
+const timerSource$ = timer(3000);
+// timerSource$.subscribe(console.log);
