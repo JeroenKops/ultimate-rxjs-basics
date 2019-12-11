331d7fe250d327002c78c469bf2452f2e0e2a9b3
JeroenKops <jeroen.kops@partners4it.nl>
2019-12-11 15:43:27 +0100

    take while operator

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import {of, fromEvent} from 'rxjs';
-import{map, first, take} from 'rxjs/operators';
-
+import{map, first, take, takeWhile} from 'rxjs/operators';
+console.clear();
 
 const clicks$ = fromEvent(document, 'click');
 
 clicks$.pipe(
   map(value => {return {x: value.clientX, y: value.clientY}}),
-  first(({x}) => x < 100)
+  takeWhile(({x}) => x < 100)
 )
 .subscribe({
   next:console.log,
   complete: () => console.log('Complete!')
-});
+}); 
