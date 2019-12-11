d7686a4153dee5b53f6731ceee44d311e439b622
JeroenKops <jeroen.kops@partners4it.nl>
2019-12-11 15:09:40 +0100

    aded lab2

--- index.ts
+++ index.ts
@@ -1,65 +1,20 @@
 console.clear();
 
-import { Observable } from "rxjs";
-import {fromEvent} from 'rxjs';
-import { of } from 'rxjs';
-import { range } from 'rxjs';
-import { from } from 'rxjs';
-import { interval } from 'rxjs';
-import { timer} from 'rxjs';
+import {interval,pipe} from 'rxjs';
+import {scan, filter, mapTo} from'rxjs/operators';
 
-const observer = {
-  next: value => console.log("next", value),
-  error: error => console.log("error", error),
-  complete: () => console.log("complete")
-};
-const observable = new Observable(subscriber => {
-  subscriber.next("Hello");
-  subscriber.next("World");
-  subscriber.next("World");
-  subscriber.next("Hello");
-  subscriber.complete();
-  subscriber.next("Hello");
+const messageDiv = document.getElementById('message');
+const countDiv = document.getElementById('count');
+
+interval(1000).pipe(
+  mapTo(-1),
+  scan((accumulator, current)=>{
+return  accumulator + current;
+  },10),
+  filter(value => value >= 0)
+).subscribe( value => {
+  countDiv.innerHTML = value;
+  if(!value){
+    messageDiv.innerHTML = 'ready for takeOff!';
+  }
 });
-
-// observable.subscribe(observer);
-
-
-const source$ =  fromEvent(document,'click');
-
-//const subOne = source$.subscribe(observer);
-//const subTwo = source$.subscribe(observer);
-
-// setTimeout(()=>{
-//   console.log('unsubscribe from one');
-//   subOne.unsubscribe();
-//   },3000)
-
-const fromSoure$ = of(1,2,3,4,5);
-// fromSoure$.subscribe(observer);
-
-const rangeSource$ = range(1,9);
-// rangeSource$.subscribe(observer);
-
-const ofSource$ = of([1,2,3,4,5]);
-// ofSource$.subscribe(observer);
-
-const fromSource$ = from([1,2,3,4,5]);
-// fromSoure$.subscribe(observer);
-
-function* hello(){
-  yield 'Hello';
-  yield 'World'
-}
-
-const fromIteratorSource$ = from(hello());
-// fromIteratorSource$.subscribe(observer);
-
-const fromUrlSource$ = from(fetch('https://api.github.com/users/octocat'));
-// fromUrlSource$.subscribe(observer);
-
-const intervalSource$ = interval(1000);
-// intervalSource$.subscribe(console.log);
-
-const timerSource$ = timer(3000);
-// timerSource$.subscribe(console.log);
