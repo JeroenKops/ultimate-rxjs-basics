8a8c999ad626be09d6d5f1dbde9a661d813fe2c8
JeroenKops <jeroen.kops@partners4it.nl>
2019-12-11 15:55:44 +0100

    takeuntil

--- index.ts
+++ index.ts
@@ -1,14 +1,36 @@
-import {of, fromEvent} from 'rxjs';
-import{map, first, take, takeWhile} from 'rxjs/operators';
+import {of, interval, fromEvent} from 'rxjs';
+import{map,mapTo, scan, takeUntil, first, take, takeWhile} from 'rxjs/operators';
 console.clear();
 
-const clicks$ = fromEvent(document, 'click');
+// const clicks$ = fromEvent(document, 'click');
 
-clicks$.pipe(
-  map(value => {return {x: value.clientX, y: value.clientY}}),
-  takeWhile(({x}) => x < 100)
-)
-.subscribe({
-  next:console.log,
-  complete: () => console.log('Complete!')
-}); 
+// clicks$.pipe(
+//   map(value => {return {x: value.clientX, y: value.clientY}}),
+//   takeWhile(({x}) => x < 100)
+// )
+// .subscribe({
+//   next:console.log,
+//   complete: () => console.log('Complete!')
+// }); 
+
+const countdown = document.getElementById('countdown');
+const message = document.getElementById('message');
+
+const abortButton = document.getElementById('abort');
+const counter$ = interval(1000);
+const abort$ = fromEvent(abortButton, 'click');
+
+counter$
+  .pipe(
+    mapTo(-1),
+    scan((accumulator, current) => {
+      return accumulator + current;
+    }, 10),
+    takeUntil(abort$)
+  )
+  .subscribe((value: any) => {
+    countdown.innerHTML = value;
+    if (!value) {
+      message.innerHTML = 'Liftoff!';
+    }
+  });
