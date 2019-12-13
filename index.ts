fced7e329d4ba9d47ab7e55f63267c882f8998e1
JeroenKops <jeroen.kops@partners4it.nl>
2019-12-13 10:34:39 +0100

    distinctuntilchanged

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import {of, interval, fromEvent} from 'rxjs';
-import{map,mapTo, scan, takeUntil, first, take, takeWhile} from 'rxjs/operators';
+import{map,mapTo, scan, takeUntil, first, take, takeWhile, distinctUntilChanged} from 'rxjs/operators';
 console.clear();
 
 // const clicks$ = fromEvent(document, 'click');
@@ -13,24 +13,32 @@ console.clear();
 //   complete: () => console.log('Complete!')
 // }); 
 
-const countdown = document.getElementById('countdown');
-const message = document.getElementById('message');
+// const countdown = document.getElementById('countdown');
+// const message = document.getElementById('message');
 
-const abortButton = document.getElementById('abort');
-const counter$ = interval(1000);
-const abort$ = fromEvent(abortButton, 'click');
+// const abortButton = document.getElementById('abort');
+// const counter$ = interval(1000);
+// const abort$ = fromEvent(abortButton, 'click');
 
-counter$
-  .pipe(
-    mapTo(-1),
-    scan((accumulator, current) => {
-      return accumulator + current;
-    }, 10),
-    takeUntil(abort$)
-  )
-  .subscribe((value: any) => {
-    countdown.innerHTML = value;
-    if (!value) {
-      message.innerHTML = 'Liftoff!';
-    }
-  });
+// counter$
+//   .pipe(
+//     mapTo(-1),
+//     scan((accumulator, current) => {
+//       return accumulator + current;
+//     }, 10),
+//     takeUntil(abort$)
+//   )
+//   .subscribe((value: any) => {
+//     countdown.innerHTML = value;
+//     if (!value) {
+//       message.innerHTML = 'Liftoff!';
+//     }
+//   });
+
+// const numbers$ = of(1,1,'1',2,3,3,3,4,5,3);
+
+// numbers$.pipe(
+//   distinctUntilChanged()
+// ).subscribe(console.log);
+
+
