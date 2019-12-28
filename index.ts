8353a8f887c289ef1da6170531ba91606f27965b
JeroenKops <jeroen.kops@partners4it.nl>
2019-12-28 13:21:43 +0100

    concat operator

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 console.clear();
 
-import {of, interval, fromEvent} from 'rxjs'
-import {startWith, endWith,mapTo,scan,tap, takeUntil, takeWhile} from 'rxjs/operators'
+import {of, interval, fromEvent, concat, empty} from 'rxjs'
+import {startWith, endWith,mapTo,scan,tap, takeUntil, takeWhile, take,delay, concat as concatOp} from 'rxjs/operators'
 
 // ------ startWith and endWith ------
 
@@ -16,28 +16,50 @@ import {startWith, endWith,mapTo,scan,tap, takeUntil, takeWhile} from 'rxjs/oper
 
 
 //elem
-const count = document.getElementById('count');
-const abort = document.getElementById('abort');
-const message = document.getElementById('message');
-const COUNTDOWN_FROM = 20;
+// const count = document.getElementById('count');
+// const abort = document.getElementById('abort');
+// const message = document.getElementById('message');
+// const COUNTDOWN_FROM = 20;
 
-// obs
-const abortClick$ = fromEvent(abort, 'click');
-const counter$ = interval(1000);
+// // obs
+// const abortClick$ = fromEvent(abort, 'click');
+// const counter$ = interval(1000);
 
 
-counter$.pipe(
-  mapTo(-1),
-  scan((accumulator, current)=>{
-    return accumulator + current;
-  },COUNTDOWN_FROM),
-  takeWhile( value => value >= 0),
-  takeUntil(abortClick$),
-  startWith(COUNTDOWN_FROM)
-).subscribe(value => {
-    count.innerHTML = value;
-    if(!value){
-      message.innerHTML = 'Liftoff!';
-    }
-  }
-);
+// counter$.pipe(
+//   mapTo(-1),
+//   scan((accumulator, current)=>{
+//     return accumulator + current;
+//   },COUNTDOWN_FROM),
+//   takeWhile( value => value >= 0),
+//   takeUntil(abortClick$),
+//   startWith(COUNTDOWN_FROM)
+// ).subscribe(value => {
+//     count.innerHTML = value;
+//     if(!value){
+//       message.innerHTML = 'Liftoff!';
+//     }
+//   }
+// );
+
+// ------ concat ------
+// const interval$ = interval(1000);
+
+// concat(
+//   interval$.pipe(take(3)), 
+//   interval$.pipe(take(2))
+// ).subscribe(console.log);
+
+// ------ Beter niet de concat operator gebruiken , maar de observable create concat 
+const interval$ = interval(1000);
+const delayed$ = empty().pipe(delay(1000));
+
+delayed$.pipe(
+ concatOp(
+   delayed$.pipe(startWith('3....')),
+   delayed$.pipe(startWith('2....')),
+   delayed$.pipe(startWith('1....')),
+ ),
+ startWith('Get ready!'),
+ endWith('Go!')
+).subscribe(console.log);
